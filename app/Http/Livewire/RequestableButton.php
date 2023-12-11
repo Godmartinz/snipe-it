d5f67512a10fd56d060332f4f6727675a57e7269
Godfrey M <godmartinz@gmail.com>
2023-12-11 11:06:31 -0800

    adds requestablebutton component

---
+++ app/Http/Livewire/RequestableButton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RequestableButton extends Component
+{
+    public function render()
+    {
+        return view('livewire.requestable-button');
+    }
+}
