fb744f71a25dc0a906f4fa4abbcef138f8e2ebaa
Godfrey M <godmartinz@gmail.com>
2026-01-15 11:17:26 -0800

    working on query, need to switch rn

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -843,7 +843,7 @@ class Helper
 
             $asset = new Asset();
             $total_owned = $asset->where('model_id', '=', $asset_model->id)->count();
-            $avail = $asset->where('model_id', '=', $asset_model->id)->whereIn('status_id', [1])->count();
+//            $avail = $asset->where('model_id', '=', $asset_model->id)->with('assetstatus')->where('deployable', '=', 1)->count();
 
             if ($avail <= ($asset_model->min_amt) + $alert_threshold) {
                 if ($avail > 0) {
